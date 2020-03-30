6c4438becd68cdcfa60b1b56a46581d41e32dfcd
Tahlil1 <bsse0803@iit.du.ac.bd>
2020-03-31 01:25:25 +0600

    added patient's box

--- COVID_Pred/settings.py
+++ COVID_Pred/settings.py
@@ -16,6 +16,7 @@ import os
 # modelFile = 'C:/Users/Du/Downloads/server/COVID_XRAY_nor.h5'
 # model = load_model(modelFile)
 # MODEL = model
+MODEL
 ID_TEST = 0
 ID_SUBMIT = 0
 SUBMIT_LOC_POSITIVE = "C:/Users/Du/Downloads/server/submitted_image/HasCovid19/"
