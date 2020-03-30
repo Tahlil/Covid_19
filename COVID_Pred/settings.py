fe662893eabcd59aeae4e112d6035dc9599adc1c
Tahlil <bsse0803@iit.du.ac.bd>
2020-03-30 17:30:29 +0000

    minor responsive issue fixed

--- COVID_Pred/settings.py
+++ COVID_Pred/settings.py
@@ -15,7 +15,7 @@ import os
 
 # modelFile = 'C:/Users/Du/Downloads/server/COVID_XRAY_nor.h5'
 # model = load_model(modelFile)
-# MODEL = modelHasNotCovid19
+# MODEL = model
 ID_TEST = 0
 ID_SUBMIT = 0
 SUBMIT_LOC_POSITIVE = "C:/Users/Du/Downloads/server/submitted_image/HasCovid19/"
