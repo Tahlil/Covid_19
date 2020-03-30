b1d4cc19d78a55f01efc78c91464f7485f386757
Tahlil <bsse0803@iit.du.ac.bd>
2020-03-30 12:56:56 +0000

    file upload on submit data works

--- predApp/static/js/submit.js
+++ predApp/static/js/submit.js
@@ -10,7 +10,7 @@ hideImage = function() {
     imageHolder.css('visibility', 'hidden');
 }
 showImage = function() {
-    imageHolder.style.visibility = 'visible';
+    imageHolder.css('visibility', 'visible');
 }
 
 function isInvalidAge(age) {
@@ -24,66 +24,101 @@ jQuery('input[type="radio"]').on('change', function() {
 jQuery('input[type="number"]').on('change', function() {
     age = this.value;
 });
-$(document).ready(function() {
-    hideImage();
-    $('#fileChoose').on('click', () => {
-        $("input[type=file]").click();
-    });
-
-    $("input[type=file]").on("change", () => {
-        showImage();
-        hasFile = true;
-        const files = evt.target.files; // FileList object
-        formData = new FormData()
-        formData.append('myFile', evt.target.files[0]);
-        formData.append('age', age);
-        formData.append('gender', gender);
-        imageHolder.src = URL.createObjectURL(evt.target.files[0]);
-
-    });
-
-    $('.ribbon').on('click', () => {
-        if (hasFile && gender != 'notSelected' && age != 'notSelected' && !isInvalidAge(age)) {
-            hideImage();
-            hasFile = false;
-            document.getElementById("overlay").style.display = "block";
-            $(".lds-ellipsis").style.display = "inline-block"
-            fetch(site + '/submit/', {
-                    method: 'POST',
-                    body: formData
-                })
-                .then(response => response.json())
-                .then(data => {
-                    $(".lds-ellipsis").style.display = "none";
-                    document.getElementById("overlay").style.display = "none";
-                    // document.getElementById("getNewFile").disabled = false;
-                    console.log(data);
-                    console.log(data.success);
-                    let addedClass = data.success ? "Success" : "Fail",
-                        removedClass = data.success ? "Fail" : "Success";
-                    let result = data.success ? "Successfull" : "  Failed";
-                    modalBody.innerHTML = " Submission " + result;
-                    //document.getElementById('res').innerHTML = " Test " + result;
-                    $(".modal-content").classList.add(data.hasCorona ? "redBorder" : "greenBorder");
-                    $(".modal-content").classList.add(addedClass);
-                    $(".modal-content").classList.remove(removedClass);
-                    modalBody.classList.add(addedClass);
-                    modalBody.classList.remove(removedClass);
-                    document.getElementById('mBtn').click();
-                })
-                .catch(error => {
-                    console.error(error)
-                });
-        } else {
-            $('#err-btn').click();
-            setTimeout(() => {
-                $('#exit-btn').click();
-            }, 4000);
-        }
-
-    });
 
 
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result)
+                .width(150)
+                .height(200);
+
+                formData = new FormData()
+                formData.append('myFile', e.target.result);
+                formData.append('age', age);
+                formData.append('gender', gender);
+                showImage();
+        };
+
+        reader.readAsDataURL(input.files[0]);
+       
+    }
+}
+hideImage();
+document.getElementById("fileChoose").addEventListener("click", () => {
+    console.log("Choose file");
+    $("#fl").click();
+  });
+
+// $(document).ready(function() {
+   
+//     console.log("Document ready.");
+//     // document.getElementById("fl").addEventListener("click", evt => {
+//     //     evt.preventDefault();
+//     // });
+
+//     // $("input[type=file]").on("change", (evt) => {
+//     //     console.log("Handle image");
+        
+//     //     hasFile = true;
+//     //     const files = evt.target.files; // FileList object
+//     //     formData = new FormData()
+//     //     formData.append('myFile', evt.target.files[0]);
+//     //     formData.append('age', age);
+//     //     formData.append('gender', gender);
+//     //     console.log(evt.target.files[0]);
+//     //     console.log(imageHolder);
+//     //     imageHolder.src = URL.createObjectURL(evt.target.files[0]);
+//     //     showImage();
+//     // });
+
+//     // $('.ribbon').on('click', () => {
+//     //     console.log("Ribbon clicked");
+        
+//     //     if (hasFile && gender != 'notSelected' && age != 'notSelected' && !isInvalidAge(age)) {
+//     //         hideImage();
+//     //         hasFile = false;
+//     //         document.getElementById("overlay").style.display = "block";
+//     //         $(".lds-ellipsis").style.display = "inline-block"
+//     //         fetch(site + '/submit/', {
+//     //                 method: 'POST',
+//     //                 body: formData
+//     //             })
+//     //             .then(response => response.json())
+//     //             .then(data => {
+//     //                 $(".lds-ellipsis").style.display = "none";
+//     //                 document.getElementById("overlay").style.display = "none";
+//     //                 // document.getElementById("getNewFile").disabled = false;
+//     //                 console.log(data);
+//     //                 console.log(data.success);
+//     //                 let addedClass = data.success ? "Success" : "Fail",
+//     //                     removedClass = data.success ? "Fail" : "Success";
+//     //                 let result = data.success ? "Successfull" : "  Failed";
+//     //                 modalBody.innerHTML = " Submission " + result;
+                 
+//     //                 $(".modal-content").classList.add(data.hasCorona ? "redBorder" : "greenBorder");
+//     //                 $(".modal-content").classList.add(addedClass);
+//     //                 $(".modal-content").classList.remove(removedClass);
+//     //                 modalBody.classList.add(addedClass);
+//     //                 modalBody.classList.remove(removedClass);
+//     //                 document.getElementById('mBtn').click();
+//     //             })
+//     //             .catch(error => {
+//     //                 console.error(error)
+//     //             });
+//     //     } else {
+//     //         $('#err-btn').click();
+//     //         setTimeout(() => {
+//     //             $('#exit-btn').click();
+//     //         }, 4000);
+//     //     }
+
+//     // });
 
 
-});
+
+
+// });
